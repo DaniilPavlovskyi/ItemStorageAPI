d626bcc789e5fa87aa4d0c76cafe196f1ab5cbad
DaniilPavlovskyi <daniil.pavlovskyi@gmail.com>
2024-08-19 02:41:31 +0200

    Delete target files

--- src/main/java/org/molekula/itemstorage/commands/LoadItemCommand.java
+++ src/main/java/org/molekula/itemstorage/commands/LoadItemCommand.java
@@ -5,7 +5,6 @@ import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
-import org.molekula.criminalactivity.CriminalActivity;
 import org.molekula.itemstorage.ItemStoragePlugin;
 
 public class LoadItemCommand implements CommandExecutor {
